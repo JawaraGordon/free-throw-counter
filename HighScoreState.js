a3347be3ca0e1e85361f7a5c17f104b9c7bf9c72
JawaraGordon <jawaragordon@icloud.com>
2023-02-23 19:43:30 -0500

    created logic to find a store highest score from arr in highScore variable

---
+++ HighScoreState.js
@@ -0,0 +1,55 @@
+const highScoreArr = [];
+let scoreboard = new Score(0, 'High Score:');
+
+const highScoreMade = document.getElementById('high-score-made');
+highScoreMade.append(scoreboard.message);
+
+// func to handle high scores
+const createHighScore = () => {
+  // create a state management system for high scores
+
+  let currentSessionHighScore = 0;
+
+  // func to add score to high score arr
+  const addScore = (score) => {
+    highScoreArr.push(score);
+    if (score > currentSessionHighScore) {
+      currentSessionHighScore = score;
+    }
+  };
+  // search for highest score in arr
+  const getHighScore = () => {
+    const highScoreFromArr = Math.max(...highScoreArr);
+    console.log('inside func', highScoreFromArr);
+    return highScoreFromArr;
+  };
+
+  const getCurrentSessionHighScore = () => {
+    return currentSessionHighScore;
+  };
+
+  return {
+    addScore,
+    getHighScore,
+    getCurrentSessionHighScore,
+  };
+};
+
+// select form from DOM
+const submitFormBtn = document.getElementById('form');
+console.log(submitFormBtn);
+
+// add event handler
+submitFormBtn.addEventListener('submit', function (e) {
+  e.preventDefault();
+  const scoreNum = document.getElementById('made').value;
+  // const highScore = document.getElementById('high-score');
+  // add score to the scoreboard
+
+  createHighScore().addScore(scoreNum);
+  console.log('scoreNum', scoreNum);
+  console.log('scoreArr', highScoreArr);
+  let highScore = createHighScore().getHighScore();
+  console.log('highScore', highScore);
+  // highScore.append(scoreboard.count)
+});
